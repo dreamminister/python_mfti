63f944b8d74395aa2eff05a38e02ba42e7bb2e82
Zakhar Berkut <dreamminister@users.noreply.github.com>
2018-02-06 18:33:13 +0200

    added reduce method

--- LineSum.py
+++ LineSum.py
@@ -1,16 +1,14 @@
 # https://www.coursera.org/learn/programming-in-python/programming/Mcs6i/summa-tsifr-v-strokie
 import sys
+from functools import reduce
 
 def get_string_digit_sum(digit_string):
+    digit_string = str(digit_string)
     if not digit_string.isdigit():
         print("Bad input number")
         return
 
-    sum = 0
-    for digit in digit_string:
-        sum += int(digit)
-
-    return sum
+    return reduce(lambda x,y: int(x) + int(y), digit_string)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
